a074d59d0cc808a1d385398dbb2be512afaab590
emmanuel211 <emmanuelrir02@gmail.com>
2024-03-01 23:45:42 +0300

    added user_input.py

---
+++ python/user_input.py
@@ -0,0 +1,5 @@
+name = input("please enter your name")
+age = input("please enter your age")
+location = input("please enter your location")
+
+print(f"Hello {name} you are {age} years old and live in {location}")
